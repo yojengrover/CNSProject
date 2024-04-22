66d9b294649798e8ca9ae6cee4a36aca0b706f97
divij-pawar <divijpawar9@gmail.com>
2024-04-21 21:40:44 -0400

    added hash matching

--- server/routes/Auth.js
+++ server/routes/Auth.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
+const sha256 = require('./sha256');
 
 router.get('/',auth, async (req,res) => {
     try{
-     const user = await User.findById(req.user.id).select('-password');
+     const db_password_hash = await User.findById(req.user.id).select('-password');
      ///  - logic
+     // form_password is the password submitted in the form
+     const salt = bcrypt.genSalt(10);
+     const form_password_hash = req.user.password
+     form_password_hash = hashPassword(salt, form_password);
 
-
-
-
-
-
-
-
-
-
-
-
+     if (form_password_hash.length !== db_password_hash.length) {
+        return "Password not match";
+     }
+     let match = true;
+     for (let i = 0; i < form_password_hash.length; i++) {
+        if (form_password_hash.charCodeAt(i) !== db_password_hash.charCodeAt(i)) {
+            match = false;
+        }
+     }
+     if (match) {
+        return "Password match";
+     } else {
+        return "Password not match";
+     }
 
 
      //// - logic
